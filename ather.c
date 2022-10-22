be4c199d8520d53fb5a3005d89dd41212b522aec
mrkj <0xather>
2022-10-23 02:11:01 +0300

    removed MORLES in favor of the full module @sexec

--- ather.c
+++ ather.c
@@ -12,6 +12,8 @@
 static char __udef_nodename[MAX_STR];
 #include "iota.h"
 #include "lbb/little_black_book.h"
+#include <unistd.h>
+#include <pwd.h>
 
 
 
@@ -76,7 +78,6 @@ void log_p1( _ia_p1 *p1 ) {
 
 int __1cwd( _ia_p1 *p1 ) {
 #ifndef getcwd
-	#include <unistd.h>
 #endif
 	if ( getcwd( p1 -> ia_path , sizeof( p1 -> ia_path ) ) != NULL ) {
 		return 0;
@@ -86,12 +87,56 @@ int __1cwd( _ia_p1 *p1 ) {
 
 int __1sh( _ia_p1 *p1 ) {
 #ifndef getpwuid
-	#include <pwd.h>
 #endif
 	struct passwd *p = getpwuid( getuid() );
 	memcpy( p1 -> ia_shell , p -> pw_shell , strlen( p -> pw_shell ) );
 	return 0;
 }
+
+#define PATH__NAME "PATH"
+#define CONNC__NAME "CONNC"
+#define CALL__NAME "CALL"
+#define ENTRY__NAME "ENTRY"
+#define PUBK__NAME "ADDR"
+#define CONF__NAME "CONF"
+
+
+const static char ATTRIBUTES[6][10] = { 
+	PATH__NAME , 
+	CONNC__NAME , 
+	CALL__NAME , 
+	ENTRY__NAME , 
+	PUBK__NAME , 
+	CONF__NAME
+};
+
+
+
+// Basic aether execution 
+#define _AETHER_ ".aether"
+#define AETHER_HOME ".aether/"
+#define AETHER__NAME "Aether"
+#define STRING_BUFFER 100
+#define AETHER_FILE_BUFFER 1000
+#define AETHER_REPLY_MAX 2000
+#define ATTIBUTE_BUFFER 10
+#define NULL_PTR NULL
+#define HASH_LEN 33
+
+
+// LOGGING
+#define AETHER_ENTRY "\t\t\t->\033[38;2;66;5;240m%s\n\n\tcontents\n"
+#define AETHER_THREADS_NUM "\t\033[38;2;66;5;240m->\033[0;37m%d\n"
+#define AETHER_SIG "\t\t\t\033[0;37mAether\n"
+#define AETHER_PATH "\n\033[38;2;66;5;240mAether\e[0;37m- %s\n"
+#define AETHER_F_EXEC "\n\033[38;2;255;215;50mAether\e[0;37m> %s \033[38;2;115;115;115m//%x\n"
+#define AETHER_EXEC "\n\033[38;2;255;215;50mAether\e[0;37m> %s\n"
+#define AETHER_LINE "\n\033[38;2;84;84;84mAether\e[0;37m :: %s\n"
+#define AETHER_ERROR "\n\033[38;2;178;34;34mAETHER\e[0;37m : %s -> %s\n"
+#define AETHER_LES "\n\033[38;2;84;84;84mAether\033[0;37m : L E S -> \n"
+#define AETHER_LISTEN "\n\033[38;2;84;84;84mAether\e[0;37m :: listen : .aether : %d,-> \n%s\n"
+#define AETHER_EVALUATE "\n\033[38;2;84;84;84mAether\e[0;37m :: evaluate :\n\n--->\n%s\n<---\n"
+#define AETHER_SEND "\n\033[38;2;84;84;84mAether\e[0;37m :: send : %d\n"
 
 int main( int argc , char **argv ) {
 	// 0
