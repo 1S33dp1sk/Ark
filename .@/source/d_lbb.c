3a33d92d405ff1a8b12f826131a7fdea923b77b3
mr.kj <kj@0xather.com>
2023-03-05 20:04:51 +0300

    indexer header in binary as a U128 compression instead of _Q_Q_Q_ packing

--- .@/source/d_lbb.c
+++ .@/source/d_lbb.c
@@ -9,46 +9,82 @@
 #define LOG_ERR 1
 #endif
 
+#define __LBB__ K_512
+
+void log_lbb_type(lbb_t lbbt) {
+	switch(lbbt) {
+	case __proto_call: printf("lbb type {Protocol}\n"); break;
+	case __intrp_decl: printf("lbb type {Interpreter}\n"); break;
+	case __payld_entr: printf("lbb type {Payload}\n"); break;
+	default: printf("lbb type {Unknown}\n"); break;
+	};
+};
+
+void *__search_r(char const *rname, lbb_t entry_type) {
 
 
-
-int __idxr_check_name(char const *__name) {
-
-
-
-
-    return 0;
+	return NULL;
 };
 
 
+void *__lbb_ref(char const *__rname) {
+	lbb_t __ltype=get_lbb_type(__rname);
+	void *temp=__search_r(__rname, __ltype);
+	return temp;
+};
 
 
-#ifdef OUTPUT
+void *__lbb_addref(char const *__rname, char const *__rval) {
+	dpoint *__dst=__stpoint(__rname, __rval);
+	if(__dst!=NULL){
+		__index_point(__dst);
+		log_dpoint(__dst);
+		return __dst;
+	};
+	free(__dst);	
+	return NULL;
+};
+
+#ifdef __LBB__
 int main(int argc,char const*argv[]) {
+char *__ref_out;
 #define __ARGC__ argc
-#define __ARGV__ *argv
-    __LBB_START__
-    __index_ixn("Mysterious");
-
-    printf("indexer #%lu", INDEX_AT);
-
+#define __ARGV__ argv
+	__LBB_START__
+	if(__ARGC__){
+		switch(__ARGC__) {
+		case 1:
+			printf("usage -> d-lbb [ATP_REF] [REFERENCE+PERM]\n");
+			return 1;
+		case 2:
+			__ref_out=__lbb_ref(__ARGV__[1]);
+			if(__ref_out==NULL) {
+				#ifdef LOG_ERR
+					printf("lbb cannot recognize reference : %s\n", __ARGV__[1]);
+				#endif
+				return 1;
+			};
+			break;
+		case 3:
+			__ref_out=__lbb_addref(__ARGV__[1], __ARGV__[2]);
+			if(__ref_out==NULL) {
+				#ifdef LOG_ERR
+				printf("lbb cannot add reference : %s\n", __ARGV__[1]);
+				#endif
+				return 1;
+			};
+			break;
+		};
+		#ifdef OUTPUT
+			printf("lbb -> \n");
+			printf("%s\n",__ref_out);
+		#endif
+		return 0;
+	};
+	return 0;
 };
 #endif
 
-
-
-
-
-// __LBB_START__
-//         if(argc!=2){
-//         #ifdef LOG_ERR
-//             __usage();
-//         #endif
-//         return 1;
-//     };
-//     char *temp=flds(argv[1]);
-//     INDEXER(temp);
-// INDEX_END("\n");
 
 
 
--- .@/source/karch_512/utypes.h
+++ .@/source/karch_512/utypes.h
@@ -14,7 +14,140 @@
 	typedef __d8 ulong;
 	typedef __t8 tlong;
 
+	enum __lbb_types {
+		__unknown,
+		__path_sys,
+		__proto_call,
+		__intrp_decl,
+		__payld_entr
+	};
+	typedef enum __lbb_types lbb_t;
 
+	enum __types_f {
+		FLD_STORAGE=0,
+		F_FIFO_LD=1,
+		FL_LOCK_D=2,
+	};
+
+	typedef enum __types_f type_f;
+
+	enum __atypes_ {
+	    at4=4,
+	    at6=6
+	};
+
+	enum __stypes {
+	    __sAF_INET=2,
+	    __sAF_INET6=30
+	};
+
+	enum __step_size {
+		__step_point=8,
+		__step_addr=16,
+		__step_sok=128,
+		__step_start=256,
+		__step_mor=512
+	};
+	typedef enum __step_size stp_size;
+
+	struct __st_points {
+		ulong __index;
+		char const *__ref;
+		char const *__name;
+	};
+	typedef struct __st_points dpoint;
+
+	struct __arc_t {
+		ulong __pid;
+		int __fork;
+		ulong __next;
+		dpoint **__points;
+	};
+	typedef struct __arc_t arc_t;
+
+	struct __sok_t {
+	    ulong aip_sockfd;
+	    ulong aip_socklen;
+	    char aip_socket[128];
+	};
+	typedef struct __sok_t aipsock;
+		
+	/**
+	 * M-structure {a.k.a mounted} **
+	                                */
+	struct __m_stat {
+	    ulong m_size;
+	    ulong m_mode;
+	    ulong m_blksz;
+	    ulong m_inn;
+	    char m_path[512];
+	};
+
+	typedef struct __m_stat m_stat;
+
+	struct __ixr_h {
+		ulong __size;
+		ulong d_count;
+		ulong checksum;
+	};
+
+	typedef struct __ixr_h ixr_h;
+
+
+	enum __fmt_t {
+		__nofmt__='\0',
+        nfmt=0,
+
+        __idxr__,
+        hdr_t=__idxr__+3, // _S_D_C_
+
+        
+        __keyval__,
+        kvi_t=__keyval__+3, // i : k : v
+
+        
+        __envvar__,
+        bas_t=__envvar__+3, // i : e = v
+
+        
+        __pathmut__,
+        pat_t=__pathmut__+3, // i : p := a
+
+        
+        __fld__,
+        pld_t=__fld__+3, // i : s =: d
+      
+        
+        __intrprt__,
+        ipr_t=__intrprt__+3, // < i >
+      
+        
+        __csok__,
+        sok_t=__csok__+3, // @s
+      
+        
+        __call__,
+        atp_t=__call__+3, // @s<i>{ p }
+    };
+    typedef enum __fmt_t fmt_t;
+
+	enum __fmt_cats {
+		__cval=4, //commands
+		__refs=8, //references
+		__intr=64, //interpreters
+		__payl=512, //payloads,
+		__lbb
+	};
+
+	typedef enum __fmt_cats cfmt;
+
+	struct __dpoint_indexes {
+	    ulong count;
+	    ulong filesize;
+	    dpoint **idxrs;
+	};
+
+	typedef struct __dpoint_indicies dices;
 
 	// supported file types
 		enum __ftypes {
@@ -32,12 +165,12 @@
 		    f_dir=4, 
 		/**
 		{a.k.a} BOOK **/
-		    lbb_1, //binary
-		    lbb_2, //
-		    lbb_3,
-		    lbb_4
+		    fld_1, //binary
+		    fld_2, //
+		    fld_3,
+		    fld_4
 		};
-	typedef enum __ftypes lbb_t;
+	typedef enum __ftypes fld_t;
 
 	// supported file sizes
 		enum __ftypes__size {
@@ -45,36 +178,46 @@
 		    __fsocket_sz=64,
 		    __ffield_sz=512,
 		    __fdir_sz=4096,
-		    __lbb1_sz=32768,
-		    __lbb2_sz=262144,
-		    __lbb3_sz=2097152,
-		    __lbb4_sz=16777216
+		    __fld1_sz=32768,
+		    __fld2_sz=262144,
+		    __fld3_sz=2097152,
+		    __fld4_sz=16777216
 		};
-	typedef enum __ftypes__size lbb_sz;
+	typedef enum __ftypes__size fld_sz;
 
-	static const char *lfiles[]={"@charms/lbb/","@charms/lock","@charms/.lesfile"};
+	
+
+static const char *__lbb_indexfile = "@charms/lbb/.lbb\0";
+static const char *__lbb_convdir = "@charms/lbb\0";
+static const char *__lbb_locking = "@charms/lock\0";
 
 	/**
 	 * 
-	 * can modify a call to 
-	 * `conifgure.ac` to iterate 
+	 * can modify a call to similar to `autoreconf` in `conifgure.ac` to iterate 
 	 * something like AC_DEFINE([__uX],[__u5],[defines a struct of 8**5])
 	 * and create a __uX where `X` is any number
 	 * for any base 8 num, obtaining the `__u64` defined by linux kernel etc.
 	 * 
 	**/
-
 		static ulong __cindex;
 
 		struct _u2st {
-			unsigned long _1;
-			unsigned long _2;
+			ulong _1;
+			ulong _2;
+			ulong _3;
 		};
-		static const struct _u2st __u0={._1=0,._2=0};
-		#define _u2s struct __u2
-		#define ret_su struct __u2 *
-		#define retSU(ret__) struct __u2 __={._1=ret__[0];._2=ret__[1]}
-		static unsigned long u[2];
-		
+		static struct _u2st ua;
+
+		#define __u2__(x) (x->_1=u[0]; x->_2=u[1];c->_3=u[2])
+		#define __u2(x) (u1__=u[0]; u2__=u[1];c->_3=u[2]) 
+
+typedef struct _u2st _u2s;
+typedef struct _u2st * pt2s;
+		static pt2s up=(pt2s)&ua;
+#define vt2s(x) (void *(x))
+		static ulong u[3];
+#define u1__ ua._1
+#define u2__ ua._2
+#define u3__ ua._3
 	#define __UTYPES__H 1
 #endif
