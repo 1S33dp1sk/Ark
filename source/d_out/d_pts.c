798d8975aa352170273f91cc0926989a03ad075f
mr.kj <kj@0xather.com>
2023-03-12 11:45:48 +0300

    #20 changing project structure

---
+++ source/d_out/d_pts.c
@@ -0,0 +1,16 @@
+/// d-pts \\\
+points 
+
+#ifndef __Karch_512__
+#include "2c/_h512.h"
+#include "2c/lbb.h"
+#include "2c/ixr.h"
+#define OUTPUT 1
+#define LOG_ERR 1
+#endif
+
+
+int main() {
+
+    
+}
