453c149d4f0f8a9e829ac18c1e909a0e567c37fa
mr.kj <kj@0xather.com>
2023-03-31 05:57:27 +0300

    #35 good results, need to compile _arch/d_*

---
+++ source/k512/constants.d
@@ -0,0 +1,11 @@
+/// machine file \\\
+K512-architecture 
+
+#ifndef __KARCH__H
+	#define d_charms "@charms/d.\0"
+	#define d_lbb "@charms/d.lbb\0"
+	#define d_atlbb "@charms/d.lbb/.lbb\0"
+	#include "../headers/standard.h"
+	#include "../headers/_512.h"
+#endif
+
